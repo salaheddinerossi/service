6d7bcfafbd252f1dcce7f7e571125585736909b1
salaheddinerossi <salaheddine.rossi@etu.uae.ac.ma>
2023-11-20 23:39:31 +0100

    co123d

--- src/main/java/com/example/testservice/controller/TestController.java
+++ src/main/java/com/example/testservice/controller/TestController.java
@@ -2,7 +2,6 @@ package com.example.testservice.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +9,7 @@ public class TestController {
 
     @GetMapping("/")
     public String helloSalah(){
-        return "hello hija 2";
+        return "hello hijaa";
+
     }
 }
